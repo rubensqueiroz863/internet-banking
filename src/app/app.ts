3445e5df95d591071b738842569e46b589e0e4a7
rubensqueiroz863 <rubensqueiroz863@exemplo.com>
2026-01-30 19:23:52 -0300

    docs(organization): Codigo organizado para inicio de projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,5 +8,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('internet-banking');
+  title = 'Internet Banking';
+
+  logout() {
+    console.log('logout');
+  }
 }
