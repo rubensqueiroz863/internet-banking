57e08c89d9b807f63e5c37fec1b6f2860768ab37
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2026-02-05 15:39:48 -0300

    feat(login) Services de login, register e auth criados e testados no pages/login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+interface AuthResponse {
+  token: string;
+  name: string;
+  cpf: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly api = 'http://localhost:8080/auth';
+
+  constructor(private readonly http: HttpClient) {}
+
+  me(): Observable<AuthResponse> {
+    return this.http.get<AuthResponse>(`${this.api}/me`);
+  }
+}
---
+++ src/app/services/register.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+interface RegisterRequest {
+  name: string;
+  cpf: string;
+  email: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+
+  private readonly api = 'http://localhost:8080/auth';
+
+  constructor(private readonly http: HttpClient) {}
+
+  register(data: RegisterRequest): Observable<string> {
+    return this.http.post(
+      `${this.api}/register`,
+      data,
+      { responseType: 'text'}
+    );
+  }
+}
