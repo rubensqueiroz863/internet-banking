cc5747bcafa9571d48b2cf9e16de6f8daefddc04
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2026-02-12 17:16:59 -0300

    feat(menuDrawer): Ajuste do menu drawer e adição de botões para login e register

--- src/app/components/menudrawer/menudrawer.ts
+++ src/app/components/menudrawer/menudrawer.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-menudrawer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menudrawer.html',
   styleUrl: './menudrawer.scss',
 })
