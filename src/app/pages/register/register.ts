698c5346ebb432be55709beacaff0b5d2dc49541
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2026-02-14 12:25:48 -0300

    feat(loginErrors): Ajuste na validação da senha, cpf e email

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegisterService } from '../../services/register.service';
 import { CpfMask } from '../../shared/directives/cpf-mask';
+import { Router } from '@angular/router';
+import { ValidarCpfService } from '../../services/validarcpf.service';
 
 @Component({
   selector: 'app-register',
@@ -14,26 +16,50 @@ export class Register {
 
   registerForm: FormGroup;
 
-  constructor(private readonly fb: FormBuilder, private readonly registerService: RegisterService) {
+  resultText = "";
+  showMessage = false;
+  constructor(private readonly fb: FormBuilder, private readonly registerService: RegisterService, private readonly router: Router, private readonly validarCpf: ValidarCpfService) {
     this.registerForm = this.fb.group({
       name: [''],
-      cpf: [''],
-      email: [''],
-      password: ['']
+
+      cpf: ['', [Validators.required, Validators.minLength(14)]],
+
+      email: ['', [Validators.required, Validators.email]],
+
+      password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]]
     });
   }
 
-  register() {
-    const formValue = this.registerForm.value;
+  onSuccess() {
+    this.router.navigate(['/home']);
+  }
 
-    formValue.cpf = formValue.cpf.replaceAll(/\D/g, '');
+  onError(message: string) {
+    this.resultText = message == "" ? "Informações inválidas." : message;
+    this.showMessage = true;
+
+    setTimeout(() => {
+      this.showMessage = false;
+    }, 3000); // 3 segundos
+  }
+
+  register() {
+    const formValue = {
+      ...this.registerForm.value,
+      cpf: this.registerForm.value.cpf.replaceAll(/\D/g, '')
+    };
+
+    if (!this.validarCpf.validar(formValue.cpf)) {
+      this.onError("CPF Inválido");
+      return;
+    };
 
     this.registerService.register(formValue).subscribe({
-      next: (res) => {
-        alert("Usuário cadastrado.")
+      next: () => {
+        this.onSuccess();
       },
       error: (err) => {
-        console.log(`Erro: ${err}`);
+        this.onError("");
       }
     });
   }
