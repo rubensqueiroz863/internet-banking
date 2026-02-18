c74a01287b3b30f0efbc8b4c82722f6ca6b5d153
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2026-02-18 17:29:03 -0300

    feat(extrato): Sistema de extrato parcialmente completo

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,12 +1,24 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { Observable, map } from "rxjs";
 
 export interface TransactionResponse {
   id: string;
   amount: number;
   type: string;
   createdAt: string;
+  fromName: string;
+  toName: string;
+  fromAccountId: string;
+  toAccountId: string;
+  month?: string;
+}
+
+export interface TransactionRequest {
+  fromAccountId: string;
+  toAccountId: string;
+  amount: number;
+  type: 'DEPOSIT' | 'WITHDRAW' | 'TRANSFER';
 }
 
 @Injectable({
@@ -18,11 +30,61 @@ export class TransactionService {
 
   constructor(private readonly http: HttpClient) {}
 
-  getAllTransactions(accountId: string): Observable<TransactionResponse[]> {
-    const token = localStorage.getItem('token');
-    if (!token) throw new Error('Token não encontrado');
-    
-    return this.http.get<TransactionResponse[]>(`${this.api}/${accountId}`);
+  createTransaction(request: TransactionRequest): Observable<TransactionResponse> {
+    return this.http.post<TransactionResponse>(this.api, request);
+  }
+
+  
+  getAllTransactionsByYear(accountId: string): Observable<TransactionResponse[]> {
+    return this.http.get<TransactionResponse[]>(`${this.api}/${accountId}`).pipe(
+      map((transactions) => {
+        const currentYear = new Date().getFullYear();
+
+        return transactions
+          .filter((t) => new Date(t.createdAt.replace(' ', 'T')).getFullYear() === currentYear)
+          .map((transaction) => {
+            const date = new Date(transaction.createdAt.replace(' ', 'T'));
+
+            const monthName = date.toLocaleString('pt-BR', {
+              month: 'long'
+            });
+
+            return {
+              ...transaction,
+              month: monthName.charAt(0).toUpperCase() + monthName.slice(1),
+            };
+          });
+      })
+    );
+  }
+
+  getLastThreeTransactions(
+    accountId: string
+  ): Observable<TransactionResponse[]> {
+
+    return this.http
+      .get<TransactionResponse[]>(`${this.api}/${accountId}`)
+      .pipe(
+        map((transactions) =>
+          transactions.map((transaction) => {
+
+            const date = new Date(
+              transaction.createdAt.replace(' ', 'T')
+            );
+
+            const monthName = date.toLocaleString('pt-BR', {
+              month: 'long'
+            });
+
+            return {
+              ...transaction,
+              month:
+                monthName.charAt(0).toUpperCase() +
+                monthName.slice(1)
+            };
+          })
+        )
+      );
   }
 
 }
